5f191143a212077a08f50e926f9e3575a675a516
Rahul.Kumar <rahul.kumar@tible.com>
2025-07-02 18:20:42 +0530

    Day 7 additional, added comment

--- src/Day_7/TwoLineStarPattern.java
+++ src/Day_7/TwoLineStarPattern.java
@@ -3,11 +3,11 @@ package Day_7;
 public class TwoLineStarPattern {
     public void twoLineStarPattern(int row){
         for(int i = 0; i < row; i++){
-            System.out.print("*");
+            System.out.print("*"); //to print first star
             for (int j = 0; j < row -2; j++){
-                System.out.print(" ");
+                System.out.print(" "); //to print spaces, which should be row - 2
             }
-            System.out.println("*");
+            System.out.println("*"); //to print last star
         }
     }
 }
