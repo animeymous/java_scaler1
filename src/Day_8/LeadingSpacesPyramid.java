4e8f9b5e3a0b7d7b27c2ce26778e433c8f201c37
Rahul.Kumar <rahul.kumar@tible.com>
2025-07-02 18:38:56 +0530

    Day 8 additional, Leading spaces pyramid

---
+++ src/Day_8/LeadingSpacesPyramid.java
@@ -0,0 +1,11 @@
+package Day_8;
+
+public class LeadingSpacesPyramid {
+    public void leadingSpacesPyramid(int rows){
+        for(int i = 1; i <= rows; i++){
+            for(int k = 1; k <= rows-i; k++) System.out.print("_");
+            for(int j = 1; j <= i; j++) System.out.print("*");
+            System.out.println();
+        }
+    }
+}
