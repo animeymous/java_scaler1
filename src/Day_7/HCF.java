2a8855f29baf8ab0f175a7458fc78aba242106c6
Rahul.Kumar <rahul.kumar@tible.com>
2025-07-02 15:34:28 +0530

    Day 7, HCF

--- src/Day_7/HCF.java
+++ src/Day_7/HCF.java
@@ -1,2 +1,16 @@
-package Day_7;public class HCF {
+package Day_7;
+
+public class HCF {
+    public static int hcf(int A, int B){
+        int min = Math.min(A, B);
+        int hcfMaxValue = Integer.MIN_VALUE;
+
+        for(int i = 1; i <= min; i++){
+            if((A % i == 0) && ((B % i) == 0)){
+                hcfMaxValue = i;
+            }
+        }
+
+        return hcfMaxValue;
+    }
 }
