84705d138d4320f22d06a9d46df58e50612005d1
Rahul.Kumar <rahul.kumar@tible.com>
2025-07-04 22:54:10 +0530

    Day 9 mandatory, added comment

--- src/Day_9/AreaofCircle.java
+++ src/Day_9/AreaofCircle.java
@@ -7,7 +7,7 @@ public class AreaofCircle {
         final float pie = 3.1416f; //took final and float for storing pie value
         float area = A * pie;
 
-        return Math.round(area * 100)/100f;
+        return Math.round(area * 100)/100f; //approach to round off till 2 decimal point
     }
 
     //function to return area of circle
