6a0ffff60365f2f4e7acf0352a80a4df20ffd938
Rahul.Kumar <rahul.kumar@tible.com>
2025-07-04 23:38:39 +0530

    Day 9 mandatory, Volume Of Sphere

--- src/Day_9/VolumeOfSphere.java
+++ src/Day_9/VolumeOfSphere.java
@@ -1,2 +1,14 @@
-package Day_9;public class VolumeOfSphere {
+package Day_9;
+
+public class VolumeOfSphere {
+
+    //function to calculate volume of sphere
+    public static double calSphereVolume(int A){
+        return (4.0 / 3.0) * Math.PI * Math.pow(A, 3);
+    }
+
+    //function to return volume of sphere
+    public double volumeOfSphere(int A){
+        return calSphereVolume(A);
+    }
 }
