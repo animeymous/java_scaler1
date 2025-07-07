305e92560cc77183923216b4ff6682310f11e901
Rahul.Kumar <rahul.kumar@tible.com>
2025-07-07 22:09:51 +0530

    Day 9 mandatory, Sum of evens!

---
+++ src/FavouriteChocolate.java
@@ -0,0 +1,13 @@
+public class FavouriteChocolate {
+
+    //this function will calculate how many number number of chocolates user can buy
+    public int calculateChocolate(int A, int B, int C){
+        int result = A/B; // total money / cost of one chocolate
+
+        return Math.min(result, C); // if no. of chocs exceeding we will return maximum chocs else whatever value we got
+    }
+
+    public int favouriteChocolate(int A, int B, int C){
+        return calculateChocolate(A, B,C);
+    }
+}
