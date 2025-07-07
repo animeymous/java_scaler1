af82082115dc15f4481106ecf7bebe64c509cdca
Rahul.Kumar <rahul.kumar@tible.com>
2025-07-08 00:24:19 +0530

    Day 9 additional, Power function

---
+++ src/Day_9/PowerFunction.java
@@ -0,0 +1,14 @@
+package Day_9;
+
+public class PowerFunction {
+
+    //function to calculate power
+    public double calculatePower(int A, int B){
+        return Math.pow(A, B);
+    }
+
+    //function to return power for given value
+    public double powerFunction(int A, int B){
+        return calculatePower(A, B);
+    }
+}
