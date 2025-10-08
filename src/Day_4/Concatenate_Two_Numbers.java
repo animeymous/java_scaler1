61e059c08ba02842c067fb0f28e9c96bcc534ea4
Vikash Mishra <workpurpose619@gmail.com>
2025-10-08 22:55:01 +0530

    Day 4, concatenate two numbers

--- src/Day_4/Concatenate_Two_Numbers.java
+++ src/Day_4/Concatenate_Two_Numbers.java
@@ -1,2 +1,45 @@
-package Day_4;public class Concatenate_Two_Numbers {
+package Day_4;
+
+public class Concatenate_Two_Numbers {
+    public void concatenate_Two_Numbers(){
+        int a = 4;
+        int b = 5;
+        System.out.println("Concatenation of "+a+" and "+b+" is "+a+b);
+    }
 }
+
+/*
+Problem Description
+
+Given two numbers A and B. Concatenate the two numbers and print it.
+
+Problem Constraints
+
+1 <= A, B <= 104
+
+Input Format
+
+There are two input lines
+The first line has a single integer A.
+The second line has a single integer B.
+
+Output Format
+
+Print in a single line the concatenated number.
+
+Example Input
+
+Input 1:-
+4
+5
+Input 2:-
+16
+2
+
+Example Output
+
+Output 1:-
+45
+Output 2:-
+162
+*/
