e1ce3e9f4162d46e25c3e22e25eff703e0e99e43
Vikash Mishra <workpurpose619@gmail.com>
2025-10-08 22:33:15 +0530

    Day 4, printing A to B

--- src/Day_4/Print_A_B_C_D_E.java
+++ src/Day_4/Print_A_B_C_D_E.java
@@ -1,2 +1,23 @@
-package Day_4;public class Print_A_B_C_D_E {
+package Day_4;
+
+public class Print_A_B_C_D_E {
+    public void print_A_B_C_D_E(){
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
+        System.out.println("D");
+        System.out.println("E");
+
+    }
 }
+
+/*
+Problem Description
+
+Print the first five letters of the English alphabet i.e. A, B, C, D and E.
+
+
+Output Format
+
+Print the characters in separate lines.
+*/
