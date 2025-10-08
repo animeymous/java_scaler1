ca3f34998aec81c24de95a555f1babcfa9e4e6fe
Vikash Mishra <workpurpose619@gmail.com>
2025-10-08 21:50:42 +0530

    Day 4, simple maths program

---
+++ src/Day_4/SimpleMaths.java
@@ -0,0 +1,20 @@
+package Day_4;
+
+public class SimpleMaths {
+    public void simpleMaths(){
+        System.out.println("Addition "+ (6+3));
+        System.out.println("Subtraction "+ (6-3));
+        System.out.println("Multiplication "+ (6*3));
+        System.out.println("Division "+(6/3));
+
+    }
+}
+
+/*
+Perform the following operations on 6 and 3 and print their respective outputs in different lines.
+
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+*/
