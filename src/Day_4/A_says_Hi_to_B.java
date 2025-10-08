7806f1c46d60c83784bf04c5307a2c44d27d7f36
Vikash Mishra <workpurpose619@gmail.com>
2025-10-08 22:31:14 +0530

    Day 4, print A says hello to B program

---
+++ src/Day_4/A_says_Hi_to_B.java
@@ -0,0 +1,31 @@
+package Day_4;
+
+public class A_says_Hi_to_B {
+    public void a_says_Hi_to_B(){
+        String a = "Ram";
+        String b = "Shyam";
+        System.out.println(a+" says Hi to "+b);
+    }
+}
+
+/*
+Problem Description
+Take two names A and B as input from the user, print "A says Hi to B" (Without quotations), where A and B are the names in input.
+
+Problem Constraints
+
+1 <= len(A), len(B) <= 15
+Characters in A and B are in lowercase English Alphabets.
+
+
+Input Format
+
+There are two input lines
+The first line has a string A.
+The second line has a string B.
+
+
+Output Format
+
+Print in a single line A says Hi to B.
+*/
---
+++ src/Day_4/Print_A_B_C_D_E.java
@@ -0,0 +1,2 @@
+package Day_4;public class Print_A_B_C_D_E {
+}
