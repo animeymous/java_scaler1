ff92f1f4208d421229e6a3f499d0044d84a337e6
Vikash Mishra <workpurpose619@gmail.com>
2025-10-08 18:01:44 +0530

    Day 4, print hello program 3

---
+++ src/Day_4/HelloWorld3.java
@@ -0,0 +1,9 @@
+package Day_4;
+
+public class HelloWorld3 {
+    public void printHello3(){
+        System.out.println("Hello");
+        System.out.println();
+        System.out.print("World !");
+    }
+}
