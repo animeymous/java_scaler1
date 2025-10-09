464799a73b6f9e07bdb693e1648beb506dd3dff4
Vikash Mishra <workpurpose619@gmail.com>
2025-10-09 13:33:34 +0530

    Day 4, total bills

---
+++ src/Day_4/Total_Bills_Value.java
@@ -0,0 +1,15 @@
+package Day_4;
+
+import java.util.Scanner;
+
+public class Total_Bills_Value {
+    public void total_Bills_Value(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Give input A");
+        int a = sc.nextInt();
+        System.out.println("Give input B");
+        int b = sc.nextInt();
+        System.out.println(a*b);
+
+    }
+}
