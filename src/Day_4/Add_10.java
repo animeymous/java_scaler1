03b54be5dd685b2452c32ea8740cd00c97f5bf34
Vikash Mishra <workpurpose619@gmail.com>
2025-10-09 12:21:45 +0530

    Day 4, Add 10 program

---
+++ src/Day_4/Add_10.java
@@ -0,0 +1,28 @@
+package Day_4;
+
+public class Add_10 {
+    public void add_10(int number){
+        System.out.println(number+10);
+    }
+}
+
+/*
+Problem Description
+You will be given an integer in the input. You need to add 10 to it and print the result to the output.
+Input Format
+One integer value in the input.
+Output Format
+Print a single integer the sum.
+Problem Constraints
+1 <= N <= 1000
+Example Input
+Input 1:-
+3
+Input 2:-
+11
+Example Output
+Output 1:-
+13
+Output 2:-
+21
+*/
