4694113e4920255f412774a88baf12995009ad50
Vikash Mishra <workpurpose619@gmail.com>
2025-10-14 23:19:03 +0530

    Day 6 mandatory,even game

--- src/Day_6/Even_Game.java
+++ src/Day_6/Even_Game.java
@@ -1,2 +1,39 @@
-package Day_6;public class Even_Game {
+package Day_6;
+
+import java.util.Scanner;
+
+public class Even_Game {
+    public void even_Game(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please provide integer value");
+
+        int num = sc.nextInt();
+        int loop = 1;
+
+        while(loop <= num){
+            if(loop % 2 == 0){
+                System.out.println(loop);
+            }
+            loop++;
+        }
+    }
 }
+
+/*
+Problem Description
+Write a program to print all even numbers from 1 to N where you have to take N as input from the user.
+Note: Use while-loop OR for-loop, according to session flow.
+
+Note: You can use int(input()) for obtaining user input of number N.
+
+Problem Constraints
+
+1 <= N <= 1000000
+Input Format
+
+A single line representing N
+Output Format
+
+All even numbers from 1 to N are separated by spaces.
+* */
