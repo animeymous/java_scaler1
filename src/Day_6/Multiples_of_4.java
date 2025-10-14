fe682748cbcc0519f5532330eb75949ee7cba8b0
Vikash Mishra <workpurpose619@gmail.com>
2025-10-14 23:19:30 +0530

    Day 6 mandatory, multiple of 4

--- src/Day_6/Multiples_of_4.java
+++ src/Day_6/Multiples_of_4.java
@@ -1,2 +1,33 @@
-package Day_6;public class Multiples_of_4 {
+package Day_6;
+
+import java.util.Scanner;
+
+public class Multiples_of_4 {
+    public void multiples_of_4(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please provide integer value");
+
+        int num = sc.nextInt();
+        int loop = 1;
+
+        while(loop <= 10){
+            System.out.println(loop*num);
+            loop++;
+        }
+    }
 }
+
+/*
+Problem Description
+Given an integer input N, print all multiples of 4 less than or equal to N.
+
+Problem Constraints
+1 <= N <= 10000
+
+Input Format
+Single line containing an integer N.
+
+Output Format
+Space separated integers representing multiples of 4 less than or equal to N.
+* */
