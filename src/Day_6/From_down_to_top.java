95431ca8eec138ae107a117318542637a0ed99b7
Vikash Mishra <workpurpose619@gmail.com>
2025-10-14 23:13:38 +0530

    Day 6, mandatory,  From top to down

--- src/Day_6/From_down_to_top.java
+++ src/Day_6/From_down_to_top.java
@@ -1,2 +1,33 @@
-package Day_6;public class From_down_to_top {
+package Day_6;
+
+import java.util.Scanner;
+
+public class From_down_to_top {
+    public void from_down_to_top(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please provide integer value");
+
+        int num = sc.nextInt();
+
+        while(num >= 1){
+            System.out.println(num);
+            num--;
+        }
+    }
 }
+
+/*
+Problem Description
+
+Write a program to print all Natural numbers from N to 1 where you have to take N as input from user
+
+Problem Constraints
+1 <= N <= 10000000
+
+Input Format
+A single line representing N
+
+Output Format
+N space separated integers from N to 1.
+* */
