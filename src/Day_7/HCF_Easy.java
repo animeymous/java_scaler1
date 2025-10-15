6120c85648cd3077c29dcf1159824c35b9fd7ffc
Vikash Mishra <workpurpose619@gmail.com>
2025-10-15 12:53:42 +0530

    Day 7, optional, HCF easy

---
+++ src/Day_7/HCF_Easy.java
@@ -0,0 +1,42 @@
+package Day_7;
+
+import java.util.Scanner;
+
+public class HCF_Easy {
+    public void hCF_Easy(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please provide number in separate lines");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int max = Math.max(a,b);
+        int hcf = 1;
+
+        for(int i = 2; i <= max; i++){
+            if(a % i == 0 && b % i == 0){
+                hcf = i;
+                break;
+            }
+        }
+
+        System.out.println(hcf);
+    }
+}
+
+/*
+Problem Description
+
+Write a program to input two integers A & B from user and print their HCF.
+
+Definition Of HCF: The HCF(Highest Common Factor) or the GCD(greatest common divisor) of two positive integers
+happens to be the largest positive integer that divides the numbers without leaving a remainder.
+
+Problem Constraints
+1 <= A,B <= 100000
+
+Input Format
+First line will contain 2 integers A and B
+
+Output Format
+An integer which is the HCF of A & B.
+* */
