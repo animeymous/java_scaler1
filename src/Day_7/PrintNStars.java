27b5fbc4b669d1731a05b27084da70e3c09ac232
Vikash Mishra <workpurpose619@gmail.com>
2025-10-15 10:18:58 +0530

    Day 7, removed all questions to do it again

--- src/Day_7/PrintNStars.java
+++
@@ -1,9 +0,0 @@
-package Day_7;
-
-public class PrintNStars {
-    public static void printNStar(int number){
-        for(int i = 0; i < number; i++){
-            System.out.print("*");
-        }
-    }
-}
