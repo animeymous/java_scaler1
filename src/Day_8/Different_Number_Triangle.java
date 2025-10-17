b3db1e25644c1c874fb3310e036c501d378b1ae3
Vikash Mishra <workpurpose619@gmail.com>
2025-10-17 23:48:48 +0530

    Day 8, Optional, Different Number Triangle

---
+++ src/Day_8/Different_Number_Triangle.java
@@ -0,0 +1,55 @@
+package Day_8;
+
+import java.util.Scanner;
+
+public class Different_Number_Triangle {
+    public void different_Number_Triangle(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please provide number");
+
+        int num = sc.nextInt();
+        int count = 1;
+
+        for(int i = num; i > 0; i--){
+            for(int j = i - 1; j > 0; j--){
+                System.out.print("0 ");
+            }
+            for(int j = 1; j <= count; j++){
+                System.out.print((i * j) + " ");
+            }
+            for(int j = i - 1; j > 0; j--){
+                System.out.print("0 ");
+            }
+            count += 2;
+            System.out.println();
+        }
+    }
+}
+
+/*
+Problem Description
+Take an integer N as input, print the corresponding Full Numeric Pyramid pattern for N.
+For example if N = 5 then pattern will be like:
+
+0 0 0 0 5 0 0 0 0
+0 0 0 4 8 12 0 0 0
+0 0 3 6 9 12 15 0 0
+0 2 4 6 8 10 12 14 0
+1 2 3 4 5 6 7 8 9
+NOTE: There should be exactly one extra space after each number for each row.
+
+Problem Constraints
+2 <= N <= 100
+
+Input Format
+First and only line of input contains a single integer N.
+
+Output Format
+Output the Full Pyramid pattern corresponding to the given N.
+
+NOTE:
+
+There is no extra space before the first integer of any row.
+There is an extra space after the last integer of any row.
+All the integers in any row are space separated.
+*/
