518df0d0420d3bd86df5634d1b9445fde6806cfa
Vikash Mishra <workpurpose619@gmail.com>
2025-10-18 23:58:53 +0530

    added comment

--- src/Day_9/Count_of_primes.java
+++ src/Day_9/Count_of_primes.java
@@ -34,6 +34,17 @@ public class Count_of_primes {
 }
 
 /*
-*
-*
+Problem Description
+You will be given an integer n. You need to return the count of prime numbers less than or equal to n.
+
+Problem Constraints
+
+0 <= n <= 10^3
+
+
+Input Format
+Single input parameter n in function.
+
+Output Format
+Return the count of prime numbers less than or equal to n.
 * */
