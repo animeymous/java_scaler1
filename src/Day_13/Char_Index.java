88ed5817cc7ab642267a355630131b03587f83ff
Vikash Mishra <workpurpose619@gmail.com>
2025-11-01 21:39:59 +0530

    Day 13, Optional, Char Index

---
+++ src/Day_13/Char_Index.java
@@ -0,0 +1,48 @@
+package Day_13;
+
+import java.util.Scanner;
+
+public class Char_Index {
+
+    //function to modify string
+    public void modifyString(String A){
+        String str = "";
+        String charStr = "abcdefghijklmnopqrstuvwxyz";
+
+        for(int i = 0; i < A.length(); i++){
+            char ch = A.charAt(i);
+            String index = (charStr.indexOf(ch)+1)+"";
+            str =str + ch+ index;
+        }
+        System.out.println("New String "+str);
+    }
+    public void char_Index(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please prove string");
+        String str = sc.next();
+
+        modifyString(str);
+    }
+}
+
+/*
+Problem Description
+
+Shivam has a string A of size N containing only lowercase characters.
+He will update the string by inserting a number after every char which will be the index of that char.
+
+For E.g.: if string is azd then it will be updated as a1z26d4 since index of a is 1, of z is 26 and of d is 4.
+
+You have to tell how the string will look like afterwards,
+
+Problem Constraints
+1 <= A.size() <= 1000
+a <= A[i] <= z
+
+Input Format
+First argument is string A.
+
+Output Format
+You have to return an string as per the question.
+*/
